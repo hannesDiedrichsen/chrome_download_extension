424cbf344ee8f3480f40e316ec1f198489202db5
227felix <felix.grenzing@studium.uni-hamburg.de>
2023-10-07 16:13:28 +0200

    added filetyp logging

--- background.js
+++ background.js
@@ -5,10 +5,22 @@ chrome.downloads.onCreated.addListener(function (downloadItem) {
     console.log('Download intercepted:', downloadItem);
 
     const url = downloadItem.finalUrl;
-    const filename = url.split("/").slice(-1);
+    const filename = url.split("/").slice(-1).toString().split("?")[0];
+    console.log(filename);
+    const filetype = filename.split(".").slice(-1).toString();
     const filesize = downloadItem.totalBytes;
-    console.log("Filename: ", filename,"URL: ", url, "Filesize: ", filesize);
+    const chromefilename = downloadItem.filename
+    console.log("chromefilename: ", chromefilename)
+    console.log("Filename: ", filename,"URL: ", url, "Filesize: ", filesize, "Filetype: ", filetype);
     chrome.downloads.cancel(downloadItem.id, function () {
         console.log('Download canceled:', filename);
     });
+    // chrome.browserAction.openPopup();
+    //chrome.runtime.sendMessage(chrome.runtime.id, {
+    //    msg: "download_request",
+    //    data: {
+    //        subject: filename,
+    //        content: url
+    //    }
+    //});
 });
