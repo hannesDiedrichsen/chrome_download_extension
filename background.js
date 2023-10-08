4da3994976dd4fded471cc5a4595c9bc27bc43ce
Hannes <hannes.diedrichsen@gmx.de>
2023-10-08 14:10:24 +0200

    Ask for filename change if file already exists

--- background.js
+++ background.js
@@ -2,4 +2,5 @@ chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
   suggest({filename: item.filename,
            conflict_action: 'prompt',
            conflictAction: 'prompt'});
+  console.log(item.filename);
 });
