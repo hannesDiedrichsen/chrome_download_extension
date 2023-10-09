1872fd3b593bb7e5c68ae75ab22a12ed7d27bd51
227felix <felix.grenzing@studium.uni-hamburg.de>
2023-10-09 13:42:44 +0200

    Added instant fileopening for pdfs

--- bg.js
+++ bg.js
@@ -1,13 +1,18 @@
+const download_folder = 'C:/Users/felix/Downloads/'
+let current_filename = ''
 chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
     // PDFs will always be overwritten to make PDF downloads smooth
     // File conflicts will open a prompt to change filename
     if (item.filename.endsWith('.pdf')) {
         console.log("Alaarm PDF!!!")
+        current_filename = item.filename
         suggest({
             filename: item.filename,
             conflictAction: 'overwrite'
         })
     } else {
+        console.log(item.filename)
+
         suggest({
             filename: item.filename,
             conflictAction: 'prompt'
@@ -18,10 +23,13 @@ chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
 
 // Open downloaded PDF
 // TODO: Find alternative as chrome does not allow to open file without user interaction
+// chrome.tabs.create({ url: download_folder + filename }); to open the file
 chrome.downloads.onChanged.addListener(function(change) {
     if (change.state && change.state.current === "complete") {
         console.log("OPEN PDF NOW")
-        chrome.downloads.open(change.id);
+
+        chrome.tabs.create({ url: download_folder + current_filename});
+        //chrome.downloads.open(change.id);
     }
 });
 
