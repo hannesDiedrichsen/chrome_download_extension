61fe2ba0f4634903e6743fb6833ca654f35cc871
Hannes <hannes.diedrichsen@gmx.de>
2023-10-09 14:26:28 +0200

    Hmmm

--- bg.js
+++ bg.js
@@ -1,11 +1,11 @@
-const download_folder = 'C:/Users/felix/Downloads/'
-let current_filename = ''
+// TODO: Store download path of user
+let absolute_path = "C:\\Users\\hanne\\Downloads\\";
 chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
     // PDFs will always be overwritten to make PDF downloads smooth
     // File conflicts will open a prompt to change filename
     if (item.filename.endsWith('.pdf')) {
         console.log("Alaarm PDF!!!")
-        current_filename = item.filename
+        absolute_path += item.filename;
         suggest({
             filename: item.filename,
             conflictAction: 'overwrite'
@@ -22,14 +22,12 @@ chrome.downloads.onDeterminingFilename.addListener(function (item, suggest) {
 
 
 // Open downloaded PDF
-// TODO: Find alternative as chrome does not allow to open file without user interaction
 // chrome.tabs.create({ url: download_folder + filename }); to open the file
 chrome.downloads.onChanged.addListener(function(change) {
-    if (change.state && change.state.current === "complete") {
+    if (change.state && change.state.current === "complete" && absolute_path.endsWith('.pdf')) {
         console.log("OPEN PDF NOW")
 
-        chrome.tabs.create({ url: download_folder + current_filename});
-        //chrome.downloads.open(change.id);
+        chrome.tabs.create({ url: absolute_path});
     }
 });
 
