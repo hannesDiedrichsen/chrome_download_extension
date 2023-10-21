5c5cf2615d8dbd063bcd079b8fe0ae43a9a7a071
Hannes <hannes.diedrichsen@gmx.de>
2023-10-21 22:32:31 +0200

    New Pictures 😎😎

--- popup.js
+++ popup.js
@@ -1,30 +1,28 @@
-
-
-
-document.getElementById('saveButton').addEventListener('click', function() {
+document.getElementById('saveButton').addEventListener('click', function () {
     let path = document.getElementById('directoryInput').value;
     // Speichern Sie den Pfad und führen Sie weitere Aktionen durch, wie z.B. das Senden an den Hintergrundprozess.
     if (!path) {
         return
-    }
-    else {
-    chrome.storage.local.set({ "path": path })
+    } else {
+        chrome.storage.local.set({"path": path})
 
-    console.log('Gespeicherter Pfad:', path);
+        console.log('Gespeicherter Pfad:', path);
     }
 });
 
 function changeBg() {
-    const bilder = ["images/successpictures/3horse.jpeg", "images/successpictures/anotherHorse.jpeg", "images/successpictures/crazyDog.jpeg", "images/successpictures/dancinghorse.jpeg"];
+    const bilder = ["images/successpictures/3horse.jpeg", "images/successpictures/anotherHorse.jpeg",
+        "images/successpictures/crazyDog.jpeg", "images/successpictures/dancinghorse.jpeg", "images/successpictures/dog1.jpeg",
+        "images/successpictures/dog2.jpeg", "images/successpictures/dog3.jpeg"];
     const zufallsIndex = Math.floor(Math.random() * bilder.length);
     document.body.style.backgroundImage = "url('" + bilder[zufallsIndex] + "')";
 }
 
-chrome.storage.local.get('path').then(function(result) {
+chrome.storage.local.get('path').then(function (result) {
     if (result.path !== undefined) {
         console.log(result);
         changeBg();
-        document.getElementById("directoryInput").placeholder=result["path"];
+        document.getElementById("directoryInput").placeholder = result["path"];
         document.getElementById("title").innerText = "Ready to Go!";
         document.getElementById("saveButton").innerText = "Edit Path";
         document.getElementById("pathHint").innerText = "Current Path:";
